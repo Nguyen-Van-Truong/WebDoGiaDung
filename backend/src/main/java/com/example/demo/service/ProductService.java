bc85764935024e591da558b22ae7788174893631
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2024-03-30 21:30:50 +0700

    fix to run 2 project create api get n product newest

--- backend/src/main/java/com/example/demo/service/ProductService.java
+++ backend/src/main/java/com/example/demo/service/ProductService.java
@@ -4,6 +4,7 @@ import com.example.demo.api.controller.ProductMediaInfo;
 import com.example.demo.api.controller.Products;
 import com.example.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,5 +17,9 @@ public class ProductService {
     }
     public List<ProductMediaInfo>getAllList(){
       return   repo.findAllProductsWithMedia();
+    }
+
+    public List<ProductMediaInfo> getNewestProducts(int count) {
+        return repo.findNewestProductsWithMedia(PageRequest.of(0, count));
     }
 }
